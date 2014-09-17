c0db32a9d2b1e408e9f345de8e937b7e5a3a7a35
Kai Wang <charles.wangkai@gmail.com>
2014-09-17 10:26:10 +0800

    P007

---
+++ P007.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class P007 {
+	static int[] primes;
+	static final int LIMIT = 10001;
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int T = in.nextInt();
+		for (int tc = 0; tc < T; tc++) {
+			int N = in.nextInt();
+			System.out.println(solve(N));
+		}
+		in.close();
+	}
+
+	static int solve(int N) {
+		if (primes == null) {
+			primes = new int[LIMIT];
+			primes[0] = 2;
+			int number = 1;
+			for (int i = 1; i < primes.length; i++) {
+				boolean isPrime;
+				do {
+					number += 2;
+					isPrime = true;
+					for (int j = 0; primes[j] * primes[j] <= number; j++) {
+						if (number % primes[j] == 0) {
+							isPrime = false;
+							break;
+						}
+					}
+				} while (!isPrime);
+				primes[i] = number;
+			}
+		}
+		return primes[N - 1];
+	}
+}
